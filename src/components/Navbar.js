73f9ed4e8df6852d2a9a9db23e3a001b9d635ba4
unknown <adorkoreunice2@gmail.com>
2021-07-12 12:55:14 +0000

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import {Navbar,Nav} from 'react-bootstrap'
 import '../styles/navbar.css'
 
 
 export default function Navbar() {
     return (
-        <div className="face">
-            <div className="links">
-                <h1 style={{textAlign:`left`,color:`white`}}>Platinum</h1>
-                <Link to ="/">Home</Link>
-                <Link to ="/about">About</Link>
-                <Link to ="/folio">Portfolio</Link>
-                <Link to ="/contact">Contact</Link>
-            </div>
-        </div>
+           <Navbar expand="md">
+               <Navbar.Brand href="/">Platinum</Navbar.Brand>
+               <Navbar.Toggle aria-controls="responsive" />
+               <Navbar.Collapse id="responsive" >
+                   <Nav as="ul">
+                    <Nav.Item as="li">
+                        <Link to="/" className="nav-link">Home</Link>
+                    </Nav.Item>
+                   </Nav>
+               </Navbar.Collapse>
+           </Navbar>
+        
     )
 }
